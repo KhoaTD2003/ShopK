ead6e9db2c7855cde207df01de43b57bdbc2eef2
DESKTOP-0VB8SL1\Dell <theddph23953@fpt.edu.vn>
2024-10-07 23:24:24 +0700

    Create entity_Hang

--- src/main/java/com/example/demo/Entities/Hang.java
+++ src/main/java/com/example/demo/Entities/Hang.java
@@ -1,4 +1,17 @@
 package com.example.demo.Entities;
 
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Table(name = "Hang")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
 public class Hang {
 }
